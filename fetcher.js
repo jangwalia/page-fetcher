404d8b769cfde7e93fb33bd2aecb8c71efe5e83e
jang walia <jangiwalia83@gmail.com>
2021-12-29 08:24:48 -0800

    refactor fetch function

--- fetcher.js
+++ fetcher.js
@@ -1,17 +1,39 @@
-const arg = process.argv;
-const url = arg[2];
-const destinationFile = arg[3];
 const request = require('request');
 const fs = require('fs');
-//get request package
-
-request(url, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  fs.writeFile(destinationFile, body, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+const readline = require('readline');
+const args = process.argv;
+const host = args[2];
+const local = args[3];
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.stdout
 });
+const pagefetcher = (add1,add2)=>{
+  request(add1,(error,response,body)=>{
+   console.log('error: ', error);
+   console.log('response: ', response && response.statusCode);
+   const bytes = response.headers["content-length"];
+   const data = body;
+   //if file exits
+   fs.access(add2,(err)=>{
+     rl.question("this file already exits..press y to overwrite..",(key)=>{
+       if(key === '\u0059'){
+        fs.writeFile('./index.html', data, (err) => {
+          if (err) throw err;
+          console.log(`Download and saved ${bytes} to ${add2}`);
+          process.exit();
+          return;
+        });
+        
+       }
+       else{
+         process.exit();
+       }
+       rl.close();
+     });
+   })
+   
+  })
+}
 
+pagefetcher(host,local);
